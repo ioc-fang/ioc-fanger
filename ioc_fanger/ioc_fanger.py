91e1fd742f07630fc6df1f067f0333acfca54013
Floyd Hightower <floyd.hightower27@gmail.com>
2018-01-24 01:05:25 -0400

    Adding local copies of json files
    Fixes #2

--- ioc_fanger/ioc_fanger.py
+++ ioc_fanger/ioc_fanger.py
@@ -3,25 +3,34 @@
 """Fang and defang indicators of compromise."""
 
 import json
+import os
 import re
 
-import requests
-
-FANG_API = "https://ioc-fang.github.io/fanging-dataset/fang.json"
-DEFANG_API = "https://ioc-fang.github.io/defanging-dataset/defang.json"
+FANG_DATA_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "./fang.json"))
+DEFANG_DATA_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "./defang.json"))
 
 
-def _get_data(api_url):
-    """."""
-    response = requests.get(api_url)
+# The code below is legacy code that I will likely remove in the future
+# import requests
+# DEFANG_DATA_URL = "https://ioc-fang.github.io/datasets/defang.json"
+# FANG_DATA_URL = "https://ioc-fang.github.io/datasets/fang.json"
+# def _get_data_from_url(url):
+#     """Get data from the given url."""
+#     response = requests.get(url)
 
-    if response.ok:
-        return json.loads(response.text)
+#     if response.ok:
+#         return json.loads(response.text)
+
+
+def _get_data_from_file(file_path):
+    """Get data from the given file path."""
+    with open(file_path, 'r') as f:
+        return json.loads(f.read())
 
 
 # get the mappings to fang/defang indicators of compromise
-fanging_mappings = _get_data(FANG_API)
-defanging_mappings = _get_data(DEFANG_API)
+fanging_mappings = _get_data_from_file(FANG_DATA_PATH)
+defanging_mappings = _get_data_from_file(DEFANG_DATA_PATH)
 
 
 def fang(text):
