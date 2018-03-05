ffe6b38d4821c6ccbfc90ad46384803930ead81b
Floyd Hightower <floyd.hightower27@gmail.com>
2018-03-05 11:36:00 -0500

    Improving tests and adding more defanging capabilities for email addresses

--- tests/test_ioc_fanger.py
+++ tests/test_ioc_fanger.py
@@ -8,29 +8,33 @@ test_ioc_fanger
 Tests for `ioc_fanger` module.
 """
 
-import json
-import os
-
 import ioc_fanger
 import pytest
 
 
 @pytest.fixture
 def defanged_text():
-    """Function to simulate command line arguments using docopt."""
     return "example[.]com hxxp://example[.]com hXXp://example[.]com example.com http://example.com hxxp://example[.]com 1[.]2[.]3[.]4 bob[@]example[.]com mary[@]example.com carlos[at]example.com juanita(at)example.com http[:]//example.org https[:]//example.org hXxps[:]//example.org/test?target=bad[@]test.com"
 
 
 @pytest.fixture
 def fanged_text():
-    """Function to simulate command line arguments using docopt."""
     return "example.com http://example.com http://example.com example.com http://example.com http://example.com 1.2.3.4 bob@example.com mary@example.com carlos@example.com juanita@example.com http://example.org https://example.org https://example.org/test?target=bad@test.com"
+
+
+@pytest.fixture
+def defanged_email_address_text():
+    return "bob[@]example.com bob(@)example.com bob[at]example.com bob(at)example.com bob@example[dot]com bob@example(dot)com"
+
+
+@pytest.fixture
+def fanged_email_address_text():
+    return "bob@example.com bob@example.com bob@example.com bob@example.com bob@example.com bob@example.com"
 
 
 def test_fanging(defanged_text, fanged_text):
     """Test fanging."""
     test_fanged_text = ioc_fanger.fang(defanged_text)
-
     assert test_fanged_text == fanged_text
 
 
@@ -43,17 +47,7 @@ def test_defanging(fanged_text):
     assert "bob@example[.]com" in defanged_text
 
 
-def _read_file(file_path):
-    """."""
-    with open(os.path.abspath(os.path.join(os.path.dirname(__file__), "../ioc_fanger/{}".format(file_path))), 'r') as f:
-        json.loads(f.read())
-
-
-def test_fanging_dataset():
-    """Make sure fang.json is in proper json."""
-    _read_file('fang.json')
-
-
-def test_defanging_dataset():
-    """Make sure defang.json is in proper json."""
-    _read_file('defang.json')
+def test_email_addresses(defanged_email_address_text, fanged_email_address_text):
+    """Make sure email addresses are properly fanged."""
+    fanged_addresses = ioc_fanger.fang(defanged_email_address_text)
+    assert fanged_addresses == fanged_email_address_text
---
+++ tests/test_json.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Make sure data files are in proper json format."""
+
+import json
+import os
+
+
+def _read_file(file_path):
+    """."""
+    with open(os.path.abspath(os.path.join(os.path.dirname(__file__), "../ioc_fanger/{}".format(file_path))), 'r') as f:
+        json.loads(f.read())
+
+
+def test_fanging_dataset():
+    """Make sure fang.json is in proper json."""
+    _read_file('fang.json')
+
+
+def test_defanging_dataset():
+    """Make sure defang.json is in proper json."""
+    _read_file('defang.json')
