7bff9f33a1353f9c7b19c638310d6e19d93e0fc9
Floyd Hightower <floyd.hightower27@gmail.com>
2019-04-02 16:57:58 -0400

    Bump version: 3.0.10 → 3.0.11

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ test_requirements = []
 
 setup(
     name='ioc_fanger',
-    version='3.0.10',
+    version='3.0.11',
     description="Python package to defang and refang indicators of compromise from text.",
     long_description=readme,
     author="Floyd Hightower",
