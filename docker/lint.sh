8b6a4198a513b39d58c32cafdfe0ed20382a0d73
Floyd Hightower <floyd.hightower27@gmail.com>
2021-03-05 07:19:33 -0500

    Adding files to use docker for development
    Fixes #39

---
+++ docker/lint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+echo "Running linters and formatters..."
+
+isort ioc_fanger/ tests/
+
+black ioc_fanger/ tests/
+
+# mypy ioc_fanger/ tests/
+
+pylint ioc_fanger/*.py
+
+flake8 ioc_fanger/ tests/
+
+bandit -r ioc_fanger/
+
+# we run black again at the end to undo any odd changes made by any of the linters above
+black ioc_fanger/ tests/
