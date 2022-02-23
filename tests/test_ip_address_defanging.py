c9c645230831c173c92e6fcd410f06a1b8b9c723
Floyd Hightower <floyd.hightower27@gmail.com>
2022-02-23 07:06:41 -0500

    Renaming file and adding more tests

--- tests/test_ip_address_defanging.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-import ioc_fanger
-
-
-@pytest.mark.parametrize("input,expected_output", [('1,2,3,4,5,6', '1,2,3,4,5,6')])
-def test_issue_71__overzealous_fanging_of_comma_seperated_nums(input, expected_output):
-    assert ioc_fanger.fang(input) == expected_output
